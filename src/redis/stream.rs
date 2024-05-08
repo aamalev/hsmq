fff483fb889eceef0d87b5b5c2f7b6e734887263
Alexander Malev <malev@somedev.ru>
2024-05-08 12:22:19 +0300

    Revert "single readers"
    This reverts commit 3118c2a1c9d14df1df75db83082852f5ee90bece.

--- src/redis/stream.rs
+++ src/redis/stream.rs
@@ -369,11 +369,15 @@ impl RedisStreamQueue {
                     let stream =
                         RedisStream::new(connection.clone(), cfg.clone(), s.clone(), name.clone());
                     ackers.insert(s.clone(), stream.clone());
-                    readers.push(stream.clone().up());
                     writers.push_back(stream);
                     unack.insert(s.clone(), UnAck::new(name.clone(), ack_timeout.clone()));
                 }
             };
+        }
+        for _ in 0..3 {
+            for s in writers.iter() {
+                readers.push(s.clone().up());
+            }
         }
         let mut tasks = JoinSet::new();
         let mut commands = JoinSet::new();
