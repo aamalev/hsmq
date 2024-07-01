9ff353bf5c54912c60295a80a653c0c936ec16a0
Alexander Malev <malev@somedev.ru>
2024-07-02 02:45:58 +0300

    utils tests

--- src/utils.rs
+++ src/utils.rs
@@ -1,11 +1,9 @@
 use crate::pb::Message;
 use std::str::FromStr;
-use std::time::SystemTime;
 pub const CREATED_AT: &str = "created-at";
 
 pub fn repr(m: &Message) -> String {
     let any = m.data.clone().unwrap_or_default();
-    let now = SystemTime::now();
     let mut result = format!("Message(topic='{}'", m.topic);
 
     if !m.key.is_empty() {
@@ -14,7 +12,7 @@ pub fn repr(m: &Message) -> String {
 
     match any.type_url.as_str() {
         "string" | "int64" => {
-            let s = String::from_utf8(any.value).unwrap();
+            let s = String::from_utf8_lossy(&any.value);
             result += format!(", data='{}'", s).as_str();
         }
         _ => (),
@@ -22,10 +20,7 @@ pub fn repr(m: &Message) -> String {
 
     if let Some(created) = m.headers.get(CREATED_AT) {
         let created = f64::from_str(created).unwrap();
-        let now = now
-            .duration_since(SystemTime::UNIX_EPOCH)
-            .unwrap()
-            .as_secs_f64();
+        let now = current_time().as_secs_f64();
         let delta = now - created;
         result += format!(", delta_µs={:.3}", delta * 1_000_000.0).as_str();
     }
@@ -44,4 +39,22 @@ pub fn current_time() -> std::time::Duration {
     std::time::SystemTime::now()
         .duration_since(std::time::UNIX_EPOCH)
         .unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{pb::Message, utils::repr};
+
+    #[tokio::test]
+    async fn repr_test() {
+        let msg = Message::default();
+        let r = repr(&msg);
+        assert!(r.len() > 0);
+    }
+
+    #[tokio::test]
+    async fn current_time_test() {
+        let ts = super::current_time();
+        assert!(!ts.is_zero());
+    }
 }
