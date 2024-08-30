3ac8134bc24441cea7fc1aa04448fb67d98815b8
Adilet Sarsembayev <adilet.sarsembayev@gmail.com>
2024-08-30 17:45:44 +0300

    Add unit test for streaming

--- src/grpc.rs
+++ src/grpc.rs
@@ -585,7 +585,7 @@ impl GrpcStreaming {
                         let consumer = GrpcConsumer::new_box(
                             self.consumer_id,
                             self.out_tx.clone(),
-                            self.server_tx.clone(),
+                               self.server_tx.clone(),
                             q,
                             prefetch_count as usize,
                             false,
@@ -699,8 +699,145 @@ fn match_for_io_error(err_status: &Status) -> Option<&std::io::Error> {
 
 #[cfg(test)]
 mod tests {
+    use tokio_stream::StreamExt;
     use crate::pb::{hsmq_server::Hsmq, Message};
-    use crate::server::{HsmqServer, Subscription};
+    use crate::server::{HsmqServer, InMemoryQueue, Subscription};
+    use crate::config;
+
+    mod tonic_mock {
+        mod mock {
+            use bytes::{Buf, BufMut, Bytes, BytesMut};
+            use http_body::{Body, Frame};
+            use prost::Message;
+            use std::{
+                collections::VecDeque,
+                marker::PhantomData,
+                pin::Pin,
+                task::{Context, Poll},
+            };
+            use tonic::{
+                codec::{DecodeBuf, Decoder},
+                Status,
+            };
+
+            #[derive(Clone)]
+            pub struct MockBody {
+                data: VecDeque<Bytes>,
+            }
+
+            impl MockBody {
+                pub fn new(data: Vec<impl Message>) -> Self {
+                    let mut queue: VecDeque<Bytes> = VecDeque::with_capacity(16);
+                    for msg in data {
+                        let buf = Self::encode(msg);
+                        queue.push_back(buf);
+                    }
+
+                    MockBody { data: queue }
+                }
+
+                pub fn is_empty(&self) -> bool {
+                    self.data.is_empty()
+                }
+
+                // see: https://github.com/hyperium/tonic/blob/1b03ece2a81cb7e8b1922b3c3c1f496bd402d76c/tonic/src/codec/encode.rs#L52
+                fn encode(msg: impl Message) -> Bytes {
+                    let mut buf = BytesMut::with_capacity(256);
+
+                    buf.reserve(5);
+                    unsafe {
+                        buf.advance_mut(5);
+                    }
+                    msg.encode(&mut buf).unwrap();
+                    {
+                        let len = buf.len() - 5;
+                        let mut buf = &mut buf[..5];
+                        buf.put_u8(0); // byte must be 0, reserve doesn't auto-zero
+                        buf.put_u32(len as u32);
+                    }
+                    buf.freeze()
+                }
+            }
+
+            impl Body for MockBody {
+                type Data = Bytes;
+                type Error = Status;
+
+                fn poll_frame(
+                    mut self: Pin<&mut Self>,
+                    _: &mut Context<'_>,
+                ) -> Poll<Option<Result<Frame<Self::Data>, Self::Error>>> {
+                    if !self.is_end_stream() {
+                        let msg = self.data.pop_front().unwrap();
+                        Poll::Ready(Some(Ok(Frame::data(msg))))
+                    } else {
+                        Poll::Ready(None)
+                    }
+                }
+
+                fn is_end_stream(&self) -> bool {
+                    self.is_empty()
+                }
+
+            }
+            /// A [`Decoder`] that knows how to decode `U`.
+            #[derive(Debug, Clone, Default)]
+            pub struct ProstDecoder<U>(PhantomData<U>);
+
+            impl<U> ProstDecoder<U> {
+                pub fn new() -> Self {
+                    Self(PhantomData)
+                }
+            }
+
+            impl<U: Message + Default> Decoder for ProstDecoder<U> {
+                type Item = U;
+                type Error = Status;
+
+                fn decode(&mut self, buf: &mut DecodeBuf<'_>) -> Result<Option<Self::Item>, Self::Error> {
+                    let item = Message::decode(buf.chunk())
+                        .map(Some)
+                        .map_err(|e| Status::internal(e.to_string()))?;
+
+                    buf.advance(buf.chunk().len());
+                    Ok(item)
+                }
+            }
+        }
+
+        use futures::{Stream, StreamExt};
+        use prost::Message;
+        use std::pin::Pin;
+        use tonic::{Request, Response, Status, Streaming};
+
+        pub use mock::{MockBody, ProstDecoder};
+        pub type StreamResponseInner<T> = Pin<Box<dyn Stream<Item = Result<T, Status>> + Send>>;
+        pub type StreamResponse<T> = Response<StreamResponseInner<T>>;
+
+        pub fn streaming_request<T>(messages: Vec<T>) -> Request<Streaming<T>>
+        where
+            T: Message + Default + 'static,
+        {
+            let body = MockBody::new(messages);
+            let decoder: ProstDecoder<T> = ProstDecoder::new();
+            let stream = Streaming::new_request(decoder, body, None, None);
+
+            Request::new(stream)
+        }
+
+        pub async fn process_streaming_response<T, F>(response: StreamResponse<T>, f: F)
+        where
+            T: Message + Default + 'static,
+            F: Fn(Result<T, Status>, usize),
+        {
+            let mut i: usize = 0;
+            let mut messages = response.into_inner();
+            while let Some(v) = messages.next().await {
+                f(v, i);
+                i += 1;
+            }
+        }
+    }
 
     #[tokio::test]
     async fn srv_publish_no_subs() {
@@ -720,5 +857,45 @@ mod tests {
         let req = tonic::Request::new(msg);
         let result = srv.publish(req).await;
         assert!(result.is_ok());
+    }
+
+    #[tokio::test]
+    async fn consume_streaming() {
+        let queue_name = "a".to_string();
+        let topic = "a.b".to_string();
+
+        let mut srv = HsmqServer::default();
+        let subscriptions = &mut srv.subscriptions;
+        let queues = &mut srv.queues;
+
+        let cfg_queue = config::InMemoryQueue{
+            name: queue_name.clone(),
+            topics: vec![topic.clone()],
+            limit: Some(99),
+            ack_timeout: config::Duration::Seconds{s: 2f32 },
+            prefetch_count: 1,
+        };
+
+        let q = InMemoryQueue::new_generic(cfg_queue.clone(), srv.task_tracker.clone());
+        let sub = subscriptions.entry(topic.clone()).or_insert_with(Subscription::new);
+        sub.subscribe(q.clone());
+        queues.insert(queue_name.clone(), q);
+
+        let mut msg = Message::default();
+        msg.topic.clone_from(&topic);
+        let req = tonic::Request::new(msg);
+        let result = srv.publish(req).await;
+        assert!(result.is_ok());
+
+        let cmd = crate::pb::SubscribeQueue {
+            queues: vec![queue_name],
+            prefetch_count: 1,
+        };
+        let kind = Some(crate::pb::request::Kind::SubscribeQueue(cmd));
+        let req = tonic_mock::streaming_request(vec![crate::pb::Request { kind }]);
+        let mut result = srv.streaming(req).await.unwrap().into_inner();
+
+        assert!(result.next().await.unwrap().is_ok());
+        assert!(result.next().await.is_none());
     }
 }
